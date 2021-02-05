1286f5c1573694eeca5a4735eb6562e42775715f
Manish Bainsla <mbainsla@localhost.localdomain>
2021-02-05 20:10:23 +0530

    added nginx proxy

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -19,13 +19,15 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+gres22y3^oml4g(qa^xag5+z_)2+bji3jq9lfp#frhd7%rer%'
+# # SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = '+gres22y3^oml4g(qa^xag5+z_)2+bji3jq9lfp#frhd7%rer%'
+SECRET_KEY = os.getenv('SECRET_KEY')
+DEBUG = os.getenv('DEBUG')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# # SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,4 +120,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = '/static/'
 STATIC_URL = '/static/'
